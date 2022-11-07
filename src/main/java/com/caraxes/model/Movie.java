5aac9659f066c89635a7bc54fc8bb9095ad5df60
bhowmik9202 <bhowmik9202@gmail.com>
2022-11-07 16:19:02 +0600

    work done on phase-01 branch

--- src/main/java/com/caraxes/model/Movie.java
+++ src/main/java/com/caraxes/model/Movie.java
@@ -1,4 +1,36 @@
 package com.caraxes.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.Year;
+
+@Entity
+@Table(name = "movie", schema = "caraxes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", unique = true)
+    private String title;
+
+    @Column(name = "rating")
+    private BigDecimal rating;
+
+    @Column(
+            name = "year_of_release",
+            columnDefinition = "smallint"
+    )
+    @Convert(
+            converter = YearAttributeConverter.class
+    )
+    private Year yearOfRelease;
 }
