daf17f708f83fdb9ed95c1e8291702051dc50054
bhowmik9202 <bhowmik9202@gmail.com>
2022-11-07 17:27:49 +0600

    work ongoing on phase-02

--- src/main/java/com/caraxes/util/HibernateUtil.java
+++ src/main/java/com/caraxes/util/HibernateUtil.java
@@ -1,6 +1,13 @@
 package com.caraxes.util;
 
+import com.caraxes.model.Movie;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
@@ -8,6 +15,25 @@ public class HibernateUtil {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory != null) {
             try {
+                Configuration configuration = new Configuration();
+
+                Properties properties = new Properties();
+
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/caraxes");
+                properties.put(Environment.USER, "root");
+                properties.put(Environment.PASS, "12345");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(Movie.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
 
             } catch (Exception e) {
                 e.printStackTrace();
