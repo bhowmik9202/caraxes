85511120326f1f58fdf489c4b50321b0cf529bfa
bhowmik9202 <bhowmik9202@gmail.com>
2022-11-08 16:46:05 +0600

    L1 cache - step 01

--- src/main/java/com/caraxes/Application.java
+++ src/main/java/com/caraxes/Application.java
@@ -1,26 +1,28 @@
 package com.caraxes;
 
-import com.caraxes.dao.MovieDAO;
 import com.caraxes.model.Movie;
 import com.caraxes.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
-import java.math.BigDecimal;
-import java.time.Year;
 
 public class Application {
     public static void main(String[] args) {
-        //Movie movie = new Movie(Long.valueOf(4), "Mystic River", BigDecimal.valueOf(7.3), Year.of(2003));
-
         Movie movie = null;
 
         Transaction transaction = null;
-        Session session1 = HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().openSession();
         try {
-            transaction = session1.beginTransaction();
-            movie = (Movie) session1.get(Movie.class, 1);
+            transaction = session.beginTransaction();
+
+            System.out.println("query about to get fired....");
+            movie = (Movie) session.get(Movie.class, Long.valueOf(1));
+            System.out.println(movie);
+
+            System.out.println("query about to get fired....");
+            movie = (Movie) session.get(Movie.class, Long.valueOf(2));
+            System.out.println(movie);
+
+            session.getTransaction().commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
