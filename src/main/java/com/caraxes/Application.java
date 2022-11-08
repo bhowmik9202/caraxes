2e811ccb0c883cb3a84c5936f625ff4244f1b534
bhowmik9202 <bhowmik9202@gmail.com>
2022-11-08 15:01:17 +0600

    working

--- src/main/java/com/caraxes/Application.java
+++ src/main/java/com/caraxes/Application.java
@@ -2,14 +2,30 @@ package com.caraxes;
 
 import com.caraxes.dao.MovieDAO;
 import com.caraxes.model.Movie;
+import com.caraxes.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.math.BigDecimal;
 import java.time.Year;
 
 public class Application {
     public static void main(String[] args) {
-        MovieDAO movieDAO = new MovieDAO();
-        Movie movie = new Movie(Long.valueOf(4), "Mystic River", BigDecimal.valueOf(7.3), Year.of(2003));
-        movieDAO.saveMovie(movie);
+        //Movie movie = new Movie(Long.valueOf(4), "Mystic River", BigDecimal.valueOf(7.3), Year.of(2003));
+
+        Movie movie = null;
+
+        Transaction transaction = null;
+        Session session1 = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session1.beginTransaction();
+            movie = (Movie) session1.get(Movie.class, 1);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/caraxes/dao/MovieDAO.java
+++ src/main/java/com/caraxes/dao/MovieDAO.java
@@ -6,24 +6,24 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class MovieDAO {
-    public void saveMovie(Movie movie) {
-        Transaction transaction = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            transaction = session.beginTransaction();
-            session.save(movie);
-
-            session.flush();
-            session.clear();
-
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
+//    public void saveMovie(Movie movie) {
+//        Transaction transaction = null;
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        try {
+//            transaction = session.beginTransaction();
+//            session.save(movie);
+//
+//            session.flush();
+//            session.clear();
+//
+//            transaction.commit();
+//        } catch (Exception e) {
+//            if (transaction != null) {
+//                transaction.rollback();
+//            }
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//    }
 }
